8e3a9d43e54ddaed2248eb4197393bb1d9b38978
jrolan-hall <j.rolan.hall@gmail.com>
2017-04-06 13:31:08 -0400

    updated arduino

--- on honu/arduino/arduino.ino
+++ on honu/arduino/arduino.ino
@@ -1,83 +1,37 @@
-#include <TextFinder.h>
-#include <Servo.h>
 #include <dht.h>
-
+#include <Servo.h>
 
 dht DHT;
-float hmd;      //humidity
-float tmp;     //temperature
-int D_SW;       //door switch
-int S_IR1;      //shell IR sensor 1
-int S_IR2;      //shell IR sensor 2
-int S_IR3;      //shell IR sensor 3
-int L_CLIFF;    //left cliff sensor
-int R_CLIFF;    //right cliff sensor
-float C_MO_V;   //voltage across comb
+Servo CMO;    //comb motor
+Servo DMO;    //door motor
+Servo LMO;    //left motor
+Servo RMO;    //right motor
+int DSW;      //door switch
+float SIR1;   //shell IR sensor 1
+float SIR2;   //shell IR sensor 2
+float SIR3;   //shell IR sensor 3
+float FCLF;   //front cliff sensor
+float BCLF;   //back cliff sensor
+float TMP;    //temperature sensor
+float HMD;    //humidity sensor
 char temp;    //temporary character for serial read
-Servo CIM_C;    //comb motor
-Servo CIM_L;    //left motor
-Servo CIM_R;    //right motor
-
- String getValue(String data, char separator, int index)
-{
-  int found = 0;
-  int strIndex[] = {0, -1};
-  int maxIndex = data.length()-1;
-
-  for(int i=0; i<=maxIndex && found<=index; i++){
-    if(data.charAt(i)==separator || i==maxIndex){
-        found++;
-        strIndex[0] = strIndex[1]+1;
-        strIndex[1] = (i == maxIndex) ? i+1 : i;
-    }
-  }
-
-  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
-}
+String toprint = "Hello Pi";
 
 void setup() {
-Serial.begin(9600);
-pinMode(2, INPUT);  //DOOR switch
-CIM_C.attach(3); //1st PWM input
-CIM_L.attach(5); //2nd PWM input
-CIM_R.attach(6); //3rd PWM input
-pinMode(4, OUTPUT); //front LED
-pinMode(7, OUTPUT); //left LED
-pinMode(8, OUTPUT); //right LED
-pinMode(10, OUTPUT); //back LED
-pinMode(9, OUTPUT); //door relay (4th PWM input)
+  Serial.begin(9600);
+  pinMode(2,INPUT);   //door switch on pin 2
+  pinMode(4,OUTPUT);  //front LED on pin 4
+  pinMode(7,OUTPUT);  //left LED on pin 7
+  pinMode(8,OUTPUT);  //right LED on pin 8
+  pinMode(10, OUTPUT); //back LED on pin 10
+  CMO.attach(3);      //comb motor on PWM input 3
+  LMO.attach(5);      //left motor on PWM input 5
+  RMO.attach(6);      //right motor on PWM input 6
+  DMO.attach(9);      //door motor on PWM input 9
 }
 
-//checkiing the sensors
-void loop() 
-  {
+void loop() {
 
-  // temperature and humidity stuff
-  hmd = DHT.humidity;
-  tmp = DHT.temperature;
-
-  //receptacle door
-  D_SW = digitalRead(2);
-
-  //is it full?
-  S_IR1 = analogRead(1);
-  S_IR2 = analogRead(2);
-  S_IR3 = analogRead(3);
-
-  //cliff sensors
-  L_CLIFF = analogRead(4);
-  R_CLIFF = analogRead(5);
-
-  //comb voltage
-  C_MO_V = analogRead(0);
-
-  //Tell Pi
-  String toprint = "HMD:"+String(hmd,2)+"|TMP:"+String(tmp,2)+"|D_SW:"+String(D_SW)+"|S_IR1:"+String(S_IR1)+"|S_IR2:"+String(S_IR2)+"|S_IR3:"+String(S_IR3)+"|L_CLIFF:"+String(L_CLIFF)+"|R_CLIFF:"+String(R_CLIFF)+"|C_MO_V:"+String(C_MO_V,3);
-  
-  Serial.println(toprint);
-
-  //Ask Pi
-  
   //get commands
   int end = 0;
   String command = "";
@@ -86,7 +40,7 @@ void loop()
     if (Serial.available() > 0) 
       {
       temp = Serial.read();
-      if(temp!='&')
+      if(temp!='\n')
         {
         command += temp;
         }
@@ -100,68 +54,135 @@ void loop()
       end=1;
      }
     }
-          
-  //separate commands
-  String CMOstr = getValue(command, '|', 0);
-  String LMOstr = getValue(command, '|', 1);
-  String RMOstr = getValue(command, '|', 2);
-  String DOORstr = getValue(command, '|', 3);
-  String FLEDstr = getValue(command, '|', 4);
-  String LLEDstr = getValue(command, '|', 5);   
-  String RLEDstr = getValue(command, '|', 6);
-  String BLEDstr = getValue(command, '|', 7);
 
-  //get values from strings
-  int CMO = CMOstr.toInt();
-  int LMO = LMOstr.toInt();
-  int RMO = RMOstr.toInt();
-  int DOOR = DOORstr.toInt();
-  int FLED = FLEDstr.toInt();
-  int LLED = LLEDstr.toInt();
-  int RLED = RLEDstr.toInt();
-  int BLED = BLEDstr.toInt();
+    int action = command.toInt();
+    int action2 = 1500;
+    
+   //figure out what to do
 
-  //actuate motors
-  CIM_C.writeMicroseconds(CMO);
-  CIM_L.writeMicroseconds(LMO);
-  CIM_R.writeMicroseconds(RMO);
-
-  //actuate door
-
-
-  //actuate LEDS
-  if(FLED!=0)
-  {
-    digitalWrite(6, HIGH);
-  }
-  else
-  {
-    digitalWrite(6, LOW);
-  }
-    if(LLED!=0)
-  {
-    digitalWrite(7, HIGH);
-  }
-  else
-  {
-    digitalWrite(7, LOW);
-  }
-    if(RLED!=0)
-  {
-    digitalWrite(8, HIGH);
-  }
-  else
-  {
-    digitalWrite(8, LOW);
-  }
-    if(BLED!=0)
-  {
-    digitalWrite(9, HIGH);
-  }
-  else
-  {
-    digitalWrite(9, LOW);
-  }
-  
-  delay(10);
+   if ((action>=1000) && (action<=1999)) //both drive motors
+    {
+      LMO.writeMicroseconds(action);
+      RMO.writeMicroseconds(action);
+      Serial.println("DRIVE:"+command);
+    }
+   else if ((action>=2000) && (action<=2999)) //left motor
+    {
+      action2 = action-1000;
+      LMO.writeMicroseconds(action2);
+      Serial.println("LMO:"+command);
+    }
+   else if ((action>=3000) && (action<=3999)) //right motor
+    {
+      action2 = action-2000;
+      RMO.writeMicroseconds(action2);
+      Serial.println("RMO:"+command);
+    }
+   else if ((action>=4000) && (action<=4999)) //comb motor
+    {
+      action2 = action-3000;
+      CMO.writeMicroseconds(action2);
+      Serial.println("CMO:"+command);
+    }
+   else if ((action>=5000) && (action<=5999)) //door motor
+    {
+      action2 = action-4000;
+      DMO.writeMicroseconds(action2);
+      Serial.println("DMO:"+command);
+    }
+   else if (action==6000) //lock position
+    {
+      digitalWrite(4, HIGH);
+      digitalWrite(7, HIGH);
+      digitalWrite(8, HIGH);
+      digitalWrite(10, HIGH);
+      CMO.writeMicroseconds(1500);
+      LMO.writeMicroseconds(1500);
+      RMO.writeMicroseconds(1500);
+      DMO.writeMicroseconds(1500);
+      Serial.println("LOCK:1");
+    }
+   else if (action==6001) //turn on front LED
+    {
+      digitalWrite(4, HIGH);
+      Serial.println("FLED:1");
+    }
+   else if (action==6002) //turn off front LED
+    {
+      digitalWrite(4, LOW);
+      Serial.println("FLED:0");
+    }
+   else if (action==6003) //turn on left LED
+    {
+      digitalWrite(7, HIGH);
+      Serial.println("LLED:1");
+    }
+   else if (action==6004) //turn off left LED
+    {
+      digitalWrite(7, LOW);
+      Serial.println("LLED:0");
+    }
+   else if (action==6005) //turn on right LED
+    {
+      digitalWrite(8, HIGH);
+      Serial.println("RLED:1");
+    }
+   else if (action==6006) //turn off right LED
+    {
+      digitalWrite(8, LOW);
+      Serial.println("RLED:0");
+    }
+   else if (action==6007) //turn on back LED
+    {
+      digitalWrite(10, HIGH);
+      Serial.println("BLED:1");
+    }
+   else if (action==6008) //turn off back LED
+    {
+      digitalWrite(10, LOW);
+      Serial.println("BLED:0");
+    }
+   else if (action==6009) //send shell IR1 data
+    {
+      SIR1 = analogRead(1);
+      toprint = "SIR1:"+String(SIR1,2);
+      Serial.println(toprint);
+    }
+   else if (action==6010) //send shell IR2 data
+    {
+      SIR2 = analogRead(2);
+      toprint = "SIR2:"+String(SIR2,2);
+      Serial.println(toprint);
+    }
+   else if (action==6011) //send shell IR3 data
+    {
+      SIR3 = analogRead(3);
+      toprint = "SIR3:"+String(SIR3,2);
+      Serial.println(toprint);
+    }
+  else if (action==6012) //send door switch data
+    {
+      DSW = digitalRead(2);
+      toprint = "DSW:"+String(DSW);
+      Serial.println(toprint);
+    }
+  else if (action==6013) //send front cliff sensor data 
+    {
+      FCLF = analogRead(10);
+      toprint = "FCLF:"+String(FCLF);
+      Serial.println(toprint);
+    }
+//  else if (action=6014) //send humidity data
+//    {
+//      HMD = DHT.humidity;
+//      toprint = "HMD:"+String(HMD);
+//      Serial.println(toprint);     
+//    }
+//  else if (action=6015) //send temperature data
+//    {
+//      TMP = DHT.temperature;
+//      toprint = "TMP:"+String(TMP);
+//      Serial.println(toprint);     
+//    }   
+    delay(10);
 }
