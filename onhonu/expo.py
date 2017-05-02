b12ecfb09c89bb940107041cd4bd499218398127
jrolan-hall <j.rolan.hall@gmail.com>
2017-05-02 15:48:25 -0400

    rthyj

--- onhonu/expo.py
+++ onhonu/expo.py
@@ -1,7 +1,8 @@
 from serial import Serial
 import threading
 from time import sleep
-
+from subprocess import call
+from random import random
 
 ser = Serial('/dev/ttyACM0', 9600)
 
@@ -9,14 +10,22 @@ def ardinput():
 	print 'listening to serial'
 	while True:
 		ln_in = ser.readline()
-		if ln_in != '':
-			print ln_in
 
 x = threading.Thread(target=ardinput)
 x.start()
 
+start_phrases = [35, 36, 37, 38, 39, 46, 47, 63, 75, 79, 110, 133]
+stop_phrases = [32, 33, 34, 80]
+dock_phrases = [10, 11, 52, 53]
+
+def pick_phrase(phrases):
+	say = int(random()*len(phrases))
+	say = 'phrases/' + str(say) + '.wav'
+	return say
+
+call["aplay", pick_phrase(start_phrases)]
+
 while True:
-#
 	print 'forwards with combs on'
 	for i in range(0,5):
 		ser.write('1700\n')
@@ -27,6 +36,7 @@ while True:
 	print 'stop'
 	for i in range(0,5): #stop
 		ser.write('6000\n')
+	call["aplay", pick_phrase(stop_phrases)]
 	sleep(5)
 
 	print 'reverse with combs off'
@@ -37,8 +47,10 @@ while True:
 	print 'stop'
 	for i in range(0,5):
 		ser.write('6000\n')
+	call["aplay", pick_phrase(stop_phrases)]
 	sleep(5)
 
+	call["aplay", pick_phrase(dock_phrases)]
 	print 'door open'
 	for i in range(0,5):
 		ser.write('5999\n')
